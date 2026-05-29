00937347365d8b72c405ac7ff10d432c24d90012
zendix <2013alievgleb@gmail.com>
2026-05-29 14:33:42 +0300

    i have done my 1 task

--- tasks/128 Longest Consecutive sequence.py
+++ tasks/128 Longest Consecutive sequence.py
@@ -1,15 +1,17 @@
-nums = set(map(int, input().split()))
-max_len = 0
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        num_set = set(nums)
+        max_len = 0
 
-for num in nums:
-    if num - 1 not in nums:
-        current_num = num
-        current_len = 1
+        for num in num_set:
+            if num - 1 not in num_set:
+                current_num = num
+                current_len = 1
 
-        while current_num + 1 in nums:
-            current_num += 1
-            current_len += 1
+                while current_num + 1 in num_set:
+                    current_num += 1
+                    current_len += 1
 
-        max_len = max(max_len, current_len)
+                max_len = max(max_len, current_len)
 
-print(max_len)
+        return max_len
